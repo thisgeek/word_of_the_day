9fc30f8e3e9696dbb5fc22f0ef6f8de91329ed8f
Christopher Joslyn <christopher.joslyn@gmail.com>
2010-01-28 09:42:03 -0500

    Added a title and reworded the link.

--- word_of_the_day.tpl.php
+++ word_of_the_day.tpl.php
@@ -1,2 +1,2 @@
 <script language="JavaScript" src="http://www.merriam-webster.com/wordoftheday.js"></script>
-<a href="http://www.merriam-webster.com/cgi-bin/mwwod.pl">MORE>></a>
+<a href="http://www.merriam-webster.com/cgi-bin/mwwod.pl">More Words</a>
